507273bb3a82673a69493f05d5d4d8e67027c772
Chris Kitching <chriskitching@linux.com>
2014-11-28 13:41:42 +0000

    Cleanup InlineMalloc and fix LLVM 3.6 compatability issues.

--- lib/vmkit/Compiler/InlineMalloc.cpp
+++ lib/vmkit/Compiler/InlineMalloc.cpp
@@ -6,6 +6,8 @@
 // License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+// A compiler pass to inline allocations and write barriers.
+//===----------------------------------------------------------------------===//
 
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/GlobalVariable.h"
@@ -13,7 +15,7 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
-#include "llvm/Support/CallSite.h"
+#include "llvm/IR/CallSite.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/DataLayout.h"
@@ -24,62 +26,67 @@
 using namespace llvm;
 
 namespace vmkit {
-
   class InlineMalloc : public FunctionPass {
-  public:
-    static char ID;
-    InlineMalloc() : FunctionPass(ID) {}
-    virtual const char* getPassName() const {
-      return "Inline malloc and barriers";
-    }
+    public:
+      static char ID;
+      InlineMalloc() : FunctionPass(ID) {}
+      virtual const char* getPassName() const {
+        return "Inline malloc and barriers";
+      }
 
-    virtual bool runOnFunction(Function &F);
-  private:
+      virtual bool runOnFunction(Function &F);
+    private:
   };
   char InlineMalloc::ID = 0;
 
+  bool InlineMalloc::runOnFunction(Function& F) {
+    Module* enclosingMod = F.getParent();
 
-bool InlineMalloc::runOnFunction(Function& F) {
-  Function* VTMalloc = F.getParent()->getFunction("VTgcmalloc");
-  Function* vmkitMalloc = F.getParent()->getFunction("vmkitgcmalloc");
-  Function* FieldWriteBarrier = F.getParent()->getFunction("fieldWriteBarrier");
-  Function* ArrayWriteBarrier = F.getParent()->getFunction("arrayWriteBarrier");
-  Function* NonHeapWriteBarrier = F.getParent()->getFunction("nonHeapWriteBarrier");
-  bool Changed = false;
-  const DataLayout *DL = getAnalysisIfAvailable<DataLayout>();
-  for (Function::iterator BI = F.begin(), BE = F.end(); BI != BE; BI++) { 
-    BasicBlock *Cur = BI; 
-    for (BasicBlock::iterator II = Cur->begin(), IE = Cur->end(); II != IE;) {
-      Instruction *I = II;
-      II++;
-      if (I->getOpcode() != Instruction::Call &&
-          I->getOpcode() != Instruction::Invoke) {
-        continue;
-      }
-      CallSite Call(I);
-      Function* Temp = Call.getCalledFunction();
-      if (Temp == VTMalloc ||
-      		Temp == vmkitMalloc) {
-        if (dyn_cast<Constant>(Call.getArgument(0))) {
-          InlineFunctionInfo IFI(NULL, DL);
-          Changed |= InlineFunction(Call, IFI);
-          break;
+    Function* VTMalloc = enclosingMod->getFunction("VTgcmalloc");
+    Function* vmkitMalloc = enclosingMod->getFunction("vmkitgcmalloc");
+
+    Function* FieldWriteBarrier = enclosingMod->getFunction("fieldWriteBarrier");
+    Function* ArrayWriteBarrier = enclosingMod->getFunction("arrayWriteBarrier");
+    Function* NonHeapWriteBarrier = enclosingMod->getFunction("nonHeapWriteBarrier");
+
+    bool Changed = false;
+    const DataLayout *DL = enclosingMod->getDataLayout();
+    for (Function::iterator BI = F.begin(), BE = F.end(); BI != BE; BI++) {
+      BasicBlock *Cur = BI;
+      for (BasicBlock::iterator II = Cur->begin(), IE = Cur->end(); II != IE;) {
+        Instruction *I = II;
+        II++;
+        if (I->getOpcode() != Instruction::Call &&
+            I->getOpcode() != Instruction::Invoke) {
+          continue;
         }
-      } else if (Temp == FieldWriteBarrier ||
-                 Temp == NonHeapWriteBarrier ||
-                 Temp == ArrayWriteBarrier) {
+
+        CallSite Call(I);
+        Function* callee = Call.getCalledFunction();
+
+        // For mallocs, we only inline if the first argument is constant.
+        if (callee == VTMalloc ||
+            callee == vmkitMalloc) {
+          if (!isa<Constant>(Call.getArgument(0))) {
+            continue;
+          }
+        } else if (callee != FieldWriteBarrier &&
+                   callee != NonHeapWriteBarrier &&
+                   callee != ArrayWriteBarrier) {
+          continue;
+        }
+
         InlineFunctionInfo IFI(NULL, DL);
         Changed |= InlineFunction(Call, IFI);
         break;
       }
     }
+
+    return Changed;
   }
-  return Changed;
-}
 
-
-FunctionPass* createInlineMallocPass() {
-  return new InlineMalloc();
-}
+  FunctionPass* createInlineMallocPass() {
+    return new InlineMalloc();
+  }
 
 }
